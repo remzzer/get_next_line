506e0fa1bc646e72ac922a9659022f66ab1597b0
remzzer <hellokiziche@gmail.com>
2020-09-09 12:14:47 +0200

    end_file

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: rrolland <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/05 09:53:00 by rrolland          #+#    #+#             */
-/*   Updated: 2020/08/07 13:02:57 by rrolland         ###   ########.fr       */
+/*   Updated: 2020/09/09 12:14:32 by rrolland         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,23 +27,17 @@ int		find_c(char *str)
 	return (-1);
 }
 
+
 int		split_store_in_line(char **store, char **line, int n)
 {
 	char	*temp;
-	//int		length;
 
 	(*store)[n] = '\0';
-	//length = ft_strlen(*store + n + 1);
-	//if (length == 0)
-	//{
-	//	free(*store);
-	//	*store = 0;
-	//	return (1);
-	//}
 	*line = ft_strdup(*store);
 	temp = ft_strdup(*store + n + 1);
 	free(*store);
 	*store = temp;
+	//printf("store: %s\n", *store);
 	return (1);
 }
 
@@ -55,15 +49,28 @@ int		get_next_line(int fd, char **line)
 	static char		*store[64];
 
 	if (read < 0 || fd < 0 || BUFFER_SIZE < 1 || !line)
+	{
 		return (-1);
+	}
 	while ((r_read = read(fd, buf, BUFFER_SIZE)) > 0)
 	{
 		buf[r_read] = '\0';
 		store[fd] = ft_strjoin(store[fd], buf);
+		//printf("buf: %s\n", buf);
+		//printf("Store[fd]: %s\n", store[fd]);
+		printf("read: %d\n", r_read);
 		if ((n = find_c(store[fd])) >= 0)
 		{
 			return (split_store_in_line(&store[fd], line, n));
 		}
+	}
+	if (store[fd] && (n = find_c(store[fd])) >= 0)
+		return (split_store_in_line(&store[fd], line, n));
+	else if (store[fd])
+	{
+		*line = store[fd];
+		free(store[fd]);
+		return (0);
 	}
 	return (0);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: rrolland <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/05 10:55:58 by rrolland          #+#    #+#             */
-/*   Updated: 2020/08/07 10:10:47 by rrolland         ###   ########.fr       */
+/*   Updated: 2020/09/09 12:05:17 by rrolland         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-#define BUFFER_SIZE 3
+#define BUFFER_SIZE 500
 
 size_t		ft_strlen(const char *str);
 char		*ft_strdup(char *src);
